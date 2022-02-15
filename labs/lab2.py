285a6cf4abe6cc138314121f9c0b3a5f100abb7f
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-15 15:32:58 +0200

    Some updates

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,3 +1,5 @@
+import time
+
 import cv2 as cv
 import numpy as np
 import imutils
@@ -5,13 +7,16 @@ import imutils
 face_cascade = cv.CascadeClassifier(f'{cv.data.haarcascades}haarcascade_frontalface_default.xml')
 smile_cascade = cv.CascadeClassifier(f'{cv.data.haarcascades}haarcascade_smile.xml')
 eye_cascade = cv.CascadeClassifier(f'{cv.data.haarcascades}haarcascade_eye.xml')
-scaling_factor = .5
+hog = cv.HOGDescriptor()
+hog.setSVMDetector(cv.HOGDescriptor_getDefaultPeopleDetector())
+scaling_factor = 0.7
 
-image = cv.imread('../data/zuckerberg.jpg')
+image = cv.imread('../data/people.jpg')
 image = cv.resize(image, None, fx=scaling_factor, fy=scaling_factor, interpolation=cv.INTER_AREA)
 gray_filter = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
 faces_coords = face_cascade.detectMultiScale(gray_filter, minNeighbors=1)
+people_recognitions = hog.detectMultiScale(image, winStride=(8, 8), padding=(30, 30))
 
 for(x, y, width, height) in faces_coords:
     cv.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
@@ -26,6 +31,24 @@ for(x, y, width, height) in faces_coords:
     for (ex, ey, ew, eh) in eye:
         cv.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 0, 255), 1)
 
-cv.imshow('Zuckerberg\'s family', image)
+cv.imshow('Soccer team', image)
 cv.waitKey(0)
-print(f'Total faces: {len(faces_coords)}')
+cv.destroyAllWindows()
+print(f'Total people: {len(people_recognitions[0])}')
+
+camera = cv.VideoCapture(0)
+
+while True:
+    _, image = camera.read()
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    faces_coords = face_cascade.detectMultiScale(gray, 1.3, 4)
+    for(x, y, width, height) in faces_coords:
+        cv.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
+    cv.imshow('Face', image)
+    key = cv.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+camera.release()
+cv.destroyAllWindows()
