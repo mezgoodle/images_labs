418afe55aca75dc0c9adc82989a57434814f0f3a
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-02-15 16:07:46 +0200

    Some updates

--- labs/lab2.py
+++ labs/lab2.py
@@ -1,8 +1,5 @@
-import time
-
 import cv2 as cv
 import numpy as np
-import imutils
 
 face_cascade = cv.CascadeClassifier(f'{cv.data.haarcascades}haarcascade_frontalface_default.xml')
 smile_cascade = cv.CascadeClassifier(f'{cv.data.haarcascades}haarcascade_smile.xml')
@@ -44,6 +41,25 @@ while True:
     faces_coords = face_cascade.detectMultiScale(gray, 1.3, 4)
     for(x, y, width, height) in faces_coords:
         cv.rectangle(image, (x, y), (x + width, y + height), (255, 0, 0), 2)
+    cv.imshow('Face', image)
+    key = cv.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+camera.release()
+cv.destroyAllWindows()
+
+video = cv.VideoCapture('../data/video.mp4')
+
+while True:
+    _, image = video.read()
+    image = cv.resize(image, (800, 560))
+    gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))
+    boxes = np.array([[x, y, x+width, y+height] for (x, y, width, height) in boxes])
+    for(x_a, y_a, x_b, y_b) in boxes:
+        cv.rectangle(image, (x_a, y_a), (x_b, y_b), (0, 255, 0), 2)
     cv.imshow('Face', image)
     key = cv.waitKey(1)
 
