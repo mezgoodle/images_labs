2a5c328a07931594802fa04816c7d16a811236c5
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-02 16:06:20 +0200

    Update lab3.py

--- labs/lab3.py
+++ labs/lab3.py
@@ -1,56 +1,38 @@
 import cv2 as cv
 import numpy as np
-import matplotlib.pylab as plt
 
 
 def process_image(image):
-    plt.imshow(image)
-    plt.show()
     grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-
-    cv.imshow('frame', grey_image)
-    cv.waitKey()
 
     kernel_size = (5, 5)
     blur_gray = cv.GaussianBlur(grey_image, kernel_size, 0)
-
-    cv.imshow('frame', blur_gray)
-    cv.waitKey()
 
     low_treshold = 50
     high_treshold = 150
     edges = cv.Canny(blur_gray, low_treshold, high_treshold)
 
-    cv.imshow('frame', edges)
-    cv.waitKey()
-
     mask = np.zeros_like(edges)
     ignore_mask_color = 255
 
     image_shape = image.shape
-    vertices = np.array([[(0, image_shape[0]), (image_shape[1] / 2, image_shape[0] / 2), (image_shape[1], image_shape[0])]], dtype=np.int32)
+    vertices = np.array([[(0, image_shape[0]), (960, 690), (1080, 690), (image_shape[1], image_shape[0])]], dtype=np.int32)
     cv.fillPoly(mask, vertices, ignore_mask_color)
     masked_edges = cv.bitwise_and(edges, mask)
-
-    cv.imshow('frame', masked_edges)
-    cv.waitKey()
 
     rho = 2
     theta = np.pi / 180
     threshold = 15
     min_line_length = 40
     max_line_gap = 30
-    line_image = np.copy(image) * 0
 
     lines = cv.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
 
 
     for line in lines:
         for x1,y1,x2,y2 in line:
-            cv.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-    cv.imshow('frame', line_image)
-    cv.waitKey()
-    return line_image
+            cv.line(image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    return image
 
 
 video_capture = cv.VideoCapture('../data/road_video.mp4')
@@ -60,7 +42,6 @@ while (video_capture.isOpened()):
     if ret:
         output = process_image(frame)
         cv.imshow('frame', output)
-        break
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
     else:
