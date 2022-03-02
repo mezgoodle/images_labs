b82e0c3e12017a54657a74f8b0a55e9bd377819e
Maxim Zavalniuk <mezgoodle@gmail.com>
2022-03-02 16:37:53 +0200

    Update lab3.py

--- labs/lab3.py
+++ labs/lab3.py
@@ -2,32 +2,47 @@ import cv2 as cv
 import numpy as np
 
 
-def process_image(image):
-    grey_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+def image_to_gray(image):
+    return cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 
+
+def blur_image(image):
     kernel_size = (5, 5)
-    blur_gray = cv.GaussianBlur(grey_image, kernel_size, 0)
+    return cv.GaussianBlur(image, kernel_size, 0)
 
+
+def find_edges(image):
     low_treshold = 50
     high_treshold = 150
-    edges = cv.Canny(blur_gray, low_treshold, high_treshold)
+    return cv.Canny(image, low_treshold, high_treshold)
 
+
+def find_masks(edges, image):
     mask = np.zeros_like(edges)
     ignore_mask_color = 255
 
     image_shape = image.shape
     vertices = np.array([[(0, image_shape[0]), (960, 690), (1080, 690), (image_shape[1], image_shape[0])]], dtype=np.int32)
     cv.fillPoly(mask, vertices, ignore_mask_color)
-    masked_edges = cv.bitwise_and(edges, mask)
+    return cv.bitwise_and(edges, mask)
 
+
+def detect_lines(masks):
     rho = 2
     theta = np.pi / 180
     threshold = 15
     min_line_length = 40
     max_line_gap = 30
 
-    lines = cv.HoughLinesP(masked_edges, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
+    return cv.HoughLinesP(masks, rho, theta, threshold, np.array([]), min_line_length, max_line_gap)
 
+
+def process_image(image):
+    grey_image = image_to_gray(image)
+    blur_gray = blur_image(grey_image)
+    edges = find_edges(blur_gray)
+    masked_edges = find_masks(edges, image)
+    lines = detect_lines(masked_edges)
 
     for line in lines:
         for x1,y1,x2,y2 in line:
